6c527e057e055a92aec6b58f8b8dc9b526348e2e
Xingzhong <xu.xeno@gmail.com>
2014-01-28 13:17:26 -0500

    fix default

--- LoanDefaultPrediction.py
+++ LoanDefaultPrediction.py
@@ -1,13 +1,59 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model, svm, preprocessing, decomposition
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from scipy.spatial import distance
+from scipy.spatial.distance import cdist
+
+def ModelDefault(data):
+    # data is a dataframe
+    # known ground truth at loss and default cols
+    scaler = preprocessing.MinMaxScaler()
+    decomp = decomposition.PCA()#n_components=150)
+    # shuffle
+    temp = data.reindex(np.random.permutation(data.index))
+    train = temp.head(int(len(temp)*0.7))
+    evals = temp.tail(int(len(temp)*0.5))
+    default_model = svm.SVC(
+        C = 1.0,
+        class_weight="auto",
+        cache_size=500, 
+        verbose=1)
+
+    x = train.iloc[:, :778].values
+    x_scaled = scaler.fit_transform(x)
+    
+    #x_lowD = decomp.fit_transform(x_scaled)
+    x_lowD = x_scaled
+    y = train.default.values
+    print "there are %s default out of %s in train data"%(train.default.sum(), train.default.count())
+    
+    default_model.fit(x_lowD, y)
+    
+    
+    x = evals.iloc[:, :778].values
+    x_scaled = scaler.transform(x)
+    x_lowD = x_scaled
+    #x_lowD = decomp.fit_transform(x_scaled)
+    evals['pred_default'] = default_model.predict(x_lowD)
+
+    print "############"
+    print "total cases %s"%len(evals)
+    print "total default %s"%evals.default.sum()
+    print "false default %s"%len(evals[(evals.pred_default==True) & (evals.default==False)])
+    print "miss default %s"%len(evals[(evals.pred_default==False) & (evals.default==True)])
+    print "positive default %s"%len(evals[(evals.pred_default==True) & (evals.default==True)])
+    print "negitive default %s"%len(evals[(evals.pred_default==False) & (evals.default==False)])
+    import pdb; pdb.set_trace()
+    
+
 
 def learning(data):
     # data is a dataframe
     # known ground truth at loss and default cols
     scaler = preprocessing.MinMaxScaler()
-    decomp = decomposition.PCA(n_components=150)
+    #decomp = decomposition.PCA(n_components=150)
     # shuffle
     temp = data.reindex(np.random.permutation(data.index))
     train = temp.head(int(len(temp)*0.7))
@@ -29,7 +75,7 @@ def learning(data):
     weight = (train.loss/50.0+1).values
 
     print "there are %s default out of %s in train data"%(train.default.sum(), train.default.count())
-    #import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     #default_model.fit(x_lowD, y)
     default_model.fit(x_lowD, y, sample_weight=weight)
     # train second linear model to do regression on default loan
@@ -75,30 +121,31 @@ def test_model(data, default_model, lrg_model, scaler, decomp):
     return data
 
 def main():
-	trainFile = "./train.tiny"
-	testFile = "./test.tiny"
+    trainFile = "./train.tiny"
+    testFile = "./test.tiny"
 
-	train = pd.read_csv(trainFile, dtype=float, index_col=0)
-	test = pd.read_csv(testFile, dtype=float, index_col=0)
-	train.index = train.index.astype(int)
-	test.index = test.index.astype(int)
-	print "finish loading data"
+    train = pd.read_csv(trainFile, dtype=float, index_col=0)
+    test = pd.read_csv(testFile, dtype=float, index_col=0)
+    train.index = train.index.astype(int)
+    test.index = test.index.astype(int)
+    print "finish loading data"
 
-	train_sample = train.copy()
-	train_sample = train_sample.fillna(train_sample.mean())
-	train_sample['default'] = train_sample.loss > 0
-	test_sample = test.copy()
-	test_sample = test_sample.fillna(test_sample.mean())
-	print "train..."
-	svm_model, lrg_model, scaler, decomp, error, evals = learning(train_sample)
-	test_result = test_model(test_sample, svm_model, lrg_model, scaler, decomp)
-	print "evaluate..."
-	
-	test_result.loss = test_result.loss.astype(int)
-	test_result.loss.to_csv('./submit.csv', header=True)
-	print "bye"
+    train_sample = train.copy()
+    train_sample = train_sample.fillna(train_sample.mean())
+    train_sample['default'] = train_sample.loss > 20
+    test_sample = test.copy()
+    test_sample = test_sample.fillna(test_sample.mean())
+    print "train..."
+    ModelDefault(train_sample)
+    #svm_model, lrg_model, scaler, decomp, error, evals = learning(train_sample)
+    #test_result = test_model(test_sample, svm_model, lrg_model, scaler, decomp)
+    #print "evaluate..."
+    #
+    #test_result.loss = test_result.loss.astype(int)
+    #test_result.loss.to_csv('./submit.csv', header=True)
+    #print "bye"
 if __name__ == '__main__':
-	main()
+    main()
 
 
 
